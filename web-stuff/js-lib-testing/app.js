6d921ee31e1ecf033f082d21f13296db0c5a2846
Omer <omerkaraman@hotmail.co.uk>
2020-01-04 13:40:05 +0000

    Did stuff

---
+++ web-stuff/js-lib-testing/app.js
@@ -0,0 +1,6 @@
+var [box, box2, box3, box4] = document.querySelectorAll(".box");
+var section = document.querySelector("section");
+draggable(box, section);
+draggable(box2, section);
+draggable(box3, section);
+draggable(box4, section);
---
+++ web-stuff/js-lib-testing/draggable.js
@@ -0,0 +1,78 @@
+//initial setup
+
+var events = new Object;
+
+if (window.PointerEvent) {
+  events.type = "pointer"
+  events.move = "pointermove";
+  events.end = "pointerup";
+  events.cancel = "pointercancel";
+} else {
+  events.type = "touch"
+  events.move = "touchmove";
+  events.end = "touchend";
+  events.cancel = "touchcancel";
+}
+
+function draggable(e, parent = document, callback = () => {}, touch_action = "none") {
+  if (e) {
+    var initialX = 0, currentX = 0, initialY = 0, currentY = 0, offsetX = 0, offsetY = 0;
+    var t_origin = [];
+
+    function onAnimFrame() {
+      offsetX = currentX - initialX;
+      offsetY = currentY - initialY;
+      e.style.transform  = 'translate(' + (offsetX - t_origin[0]) + 'px, ' + (offsetY - t_origin[1]) + 'px)';
+      requestAnimationFrame(callback);
+    }
+
+    function handleGestureMove(evt) {
+      evt.preventDefault();
+      [currentX, currentY] = getGesturePointFromEvent(evt);
+      requestAnimationFrame(onAnimFrame);
+    }
+
+    function handleGestureEnd(evt) {
+      evt.preventDefault();
+      e.style.transition = "transform 0.3s ease-out";
+      parent.removeEventListener(events.move, handleGestureMove, true);
+      parent.removeEventListener(events.end, handleGestureEnd, true);
+      parent.removeEventListener(events.cancel, handleGestureEnd, true);
+    }
+
+    function handleGestureStart(evt) {
+      evt.preventDefault();
+      e.style.transition = "initial";
+      t_origin = getOrigin(e);
+      [initialX, initialY] = getGesturePointFromEvent(evt, offsetX, offsetY);
+      parent.addEventListener(events.move, handleGestureMove, true);
+      parent.addEventListener(events.end, handleGestureEnd, true);
+      parent.addEventListener(events.cancel, handleGestureEnd, true);
+    }
+
+    function getGesturePointFromEvent(evt, xOffset = 0, yOffset = 0) {
+      var x = 0, y = 0;
+      if(evt.targetTouches) {
+        x = evt.targetTouches[0].clientX;
+        y = evt.targetTouches[0].clientY;
+      } else {
+        x = evt.clientX;
+        y = evt.clientY;
+      }
+      return [x - xOffset, y - yOffset];
+    }
+
+    function getOrigin(e) {
+      var o = window.getComputedStyle(e).transformOrigin.split(" ");
+      return [parseInt(o[0].slice(0, -2)), parseInt(o[1].slice(0, -2))];
+    }
+
+    if (events.type === "pointer") {
+      e.addEventListener('pointerdown', handleGestureStart, true);
+    }
+    else {
+      e.addEventListener('touchstart', handleGestureStart, true);
+      e.addEventListener('mousedown', handleGestureStart, true);
+    }
+  }
+}
