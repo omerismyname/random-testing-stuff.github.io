5e59978155c2d63be7046c2d16874d9060408b7c
Omer <omerkaraman@hotmail.co.uk>
2020-12-28 02:19:24 +0000

    blegh

--- web-stuff/arctic_circle/scripts/app.js
+++ web-stuff/arctic_circle/scripts/app.js
@@ -1,15 +1,14 @@
 const app = new PIXI.Application({ antialias: false, resizeTo: window });
 document.body.appendChild(app.view);
 app.renderer.resize(window.innerWidth, window.innerHeight);
+//app.renderer.backgroundColor = 0x171a1e;
 
 const graphics = new PIXI.Graphics();
 
 let MAX_SIZE = Math.floor(Math.min(window.innerWidth, window.innerHeight) / 2) * 2;
-const A = 5;
-const seed = 77;
+const A = 2;
+const seed = 124;
 const gap = 0;
-
-const coloursDict = [0,0xb01919, 0x129912, 0x1f1fd1];
 
 setTimeout(onLoad, 200);
 
@@ -20,7 +19,7 @@ function onLoad() {
   Module._generateCircle(buf, A, seed);
   let circleArr = new Uint8Array(HEAPU8.subarray(buf, buf+gridArr.length*gridArr.BYTES_PER_ELEMENT));
   Module._free(buf);
-
+  console.log(circleArr)
   drawCircle(circleArr);
 
   app.stage.addChild(graphics);
@@ -31,14 +30,21 @@ function onLoad() {
   }
 }
 
+const coloursDict = [0xebcb8b, 0xbf616a, 0xb48ead, 0x81a1c1];
+const rectangles = [[-1, 0, 2, 1], [0, 0, 1, 2], [0, 0, 2, 1], [0, -1, 1, 2]]
+
 function drawCircle(circleArr) {
   graphics.clear();
   let size = MAX_SIZE / (A*2);
   for (let y = 0; y < A*2; y++) {
     for (let x = 0; x < A*2; x++) {
-      graphics.beginFill((circleArr[2*A*y + x]) ? 0xffffff : 0);
-      graphics.drawRect(x * (size+gap), y * (size+gap), size, size);
-      graphics.endFill();
+      const d = circleArr[2*A*y + x] - 1;
+      if (d >= 0 && d < 4) {
+        const colour = coloursDict[d];
+        graphics.beginFill(colour);
+        graphics.drawRect((rectangles[d][0]+x) * (size+gap), (rectangles[d][1]+y) * (size+gap), rectangles[d][2]*size, rectangles[d][3]*size);
+        graphics.endFill();
+      }
     }
   }
 }
--- web-stuff/arctic_circle/scripts/circlegen.cpp
+++ web-stuff/arctic_circle/scripts/circlegen.cpp
@@ -33,11 +33,11 @@ int fillSquare(uint8_t* ptr, int width, int isBlack) {
   return 1;
 }
 
-int fillNewSquares(uint8_t* buf, int A) {
-  for (int y = 0; y < 2*A; y++) {
-    int off = A-y-1;
+int fillNewSquares(uint8_t* buf, int A, int currentA) {
+  for (int y = (A-currentA); y < 2*currentA-1; y++) {
+    int off = A-y-1+(A-currentA);
     if (off < 0) off = abs(off + 1);
-    for (int x = off; x < 2*A-off; x++) {
+    for (int x = off; x < 2*currentA-1-off; x++) {
       int count = 0;
       for (int sy = 0; sy < 2; sy++) {
         for (int sx = 0; sx < 2; sx++) {
@@ -73,7 +73,8 @@ int moveTile(uint8_t* buf, int x, int y, int A, int direction) {
         int target2 = buf[2*A*(y+possibleLocations[i+1]) + (x+possibleLocations[i])];
         int target3 = buf[2*A*(y+secondaryY[d]+possibleLocations[i+1]) + (x+secondaryX[d]+possibleLocations[i])];
         if (target2 > 0 && target2 < 5) {
-          moveTile(buf, x+possibleLocations[i], y+possibleLocations[i+1], A, target2);
+          if (abs(target1-direction) == 2) fillSquare((uint8_t*)(buf + (2*A*y + x)), 2*A, (2*A*y + x) % 2);
+          else moveTile(buf, x+possibleLocations[i], y+possibleLocations[i+1], A, target2);
         }
         if (target3 > 0 && target3 < 5) {
           moveTile(buf, x+secondaryX[d]+possibleLocations[i], y+secondaryY[d]+possibleLocations[i+1], A, target3);
@@ -85,22 +86,24 @@ int moveTile(uint8_t* buf, int x, int y, int A, int direction) {
 }
 
 int processMovement(uint8_t* buf, int A, int currentA) {
-  for (int y = 1; y < 2*currentA-1; y++) {
-    for (int x = 1; x < 2*currentA-1; x++) {
-      int bx = (A-1)+x;
-      int by = 2*A*((A-1)+y);
-      int d = buf[2*A*by + bx];
+  for (int y = (A-currentA); y < 2*currentA + 1; y++) {
+    for (int x = (A-currentA); x < 2*currentA + 1; x++) {
+      // int bx = (A-currentA)+x;
+      // int by = (A-currentA)+y;
+      int d = buf[2*A*y + x];
+      printf("%d, %d: %d\n", x, y, d);
       if (d > 0 && d < 5) {
-        moveTile(buf, bx, by, A, d);
+        if (currentA < 3)moveTile(buf, x, y, A, d);
       }
     }
   }
   for (int y = 0; y < A*2; y++) {
     for (int x = 0; x < A*2; x++) {
-      int bx = (A-1)+x;
-      int by = 2*A*((A-1)+y);
-      int d = buf[2*A*by + bx];
-      if (d > 0 && d < 9) buf[2*A*by + bx] = ((d-1) % 4) + 1;
+      int d = buf[2*A*y + x];
+      if (d > 0 && d < 9) {
+        buf[2*A*y + x] = ((d-1) % 4) + 1;
+        //printf("%d, %d: %d %d\n", y, x, d, ((d-1) % 4) + 1);
+      }
     }
   }
   return 1;
@@ -128,9 +131,11 @@ uint8_t generateCircle(uint8_t buf[], uint8_t A, uint8_t seed = 1) {
       //   }
       //   printf("\n");
       // }
-      processMovement(buf, A, currentA);
-      printf("-----\n");
-      fillNewSquares(buf, A);
+      //printf("-----\n");
+        processMovement(buf, A, currentA);
+      if (currentA < 3) {
+        fillNewSquares(buf, A, currentA);
+      }
       
       // for (int y = 0; y < currentA*2; y++) {
       //   for (int x = 0; x < currentA*2; x++) { 
@@ -141,12 +146,12 @@ uint8_t generateCircle(uint8_t buf[], uint8_t A, uint8_t seed = 1) {
       // }
     }
   }
-  printf("-----\n");
+  //printf("-----\n");
   for (int y = 0; y < 2*A; y++) {
     for (int x = 0; x < 2*A; x++) { 
-      printf(" %d ", buf[2*A*y+x]);
+      //printf(" %d ", buf[2*A*y+x]);
     }
-    printf("\n");
+    //printf("\n");
   }
   return 1;
 }
