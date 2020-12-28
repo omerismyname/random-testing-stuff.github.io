773abb4f165c7e8452d3fe1028bbb28db6d46bfe
Omer <omerkaraman@hotmail.co.uk>
2020-12-28 07:07:37 +0000

    bit by bit sort of

--- web-stuff/arctic_circle/scripts/circlegen.cpp
+++ web-stuff/arctic_circle/scripts/circlegen.cpp
@@ -20,7 +20,7 @@ extern "C" {
 // }
 
 int fillSquare(uint8_t* ptr, int width, int isBlack) {
-  printf("b: %d\n", isBlack);
+  //printf("b: %d\n", isBlack);
   int arr1[4] = {255, 2, 4, 255};
   int arr2[4] = {255, 1, 3, 255};
   if (rand() >> 14) {
@@ -34,20 +34,30 @@ int fillSquare(uint8_t* ptr, int width, int isBlack) {
 }
 
 int fillNewSquares(uint8_t* buf, int A, int currentA) {
-  for (int y = (A-currentA); y < 2*currentA-1; y++) {
-    int off = A-y-1+(A-currentA);
+  for (int y = 0; y < 2*currentA; y++) {
+    int off = currentA-y-1;
     if (off < 0) off = abs(off + 1);
-    for (int x = off; x < 2*currentA-1-off; x++) {
+    for (int x = off; x < 2*currentA-off; x++) {
       int count = 0;
+      int bx = (A-currentA)+x;
+      int by = (A-currentA)+y;
+      printf(" %d ", buf[2*A*by+bx]);
       for (int sy = 0; sy < 2; sy++) {
         for (int sx = 0; sx < 2; sx++) {
-          count += buf[2*A*(y+sy) + x+sx];
+          count += buf[2*A*(by+sy) + bx+sx];
+          //printf("%d, %d: %d\n", by+sy, bx+sx, buf[2*A*(by+sy) + bx+sx]);
         }
       }
       if (count == 0) {
-        fillSquare((uint8_t*)(buf + (2*A*y + x)), 2*A, (2*A*y + x) % 2);
+        fillSquare((uint8_t*)(buf + (2*A*by + x)), 2*A, (2*A*by + bx) % 2);
+        for (int sy = 0; sy < 2; sy++) {
+          for (int sx = 0; sx < 2; sx++) {
+            //printf("%d, %d: %d\n", by+sy, bx+sx, buf[2*A*(by+sy) + bx+sx]);
+          }
+        }
       }
     }
+    printf("\n");
   }
   return 1;
 }
@@ -56,10 +66,12 @@ int moveTile(uint8_t* buf, int x, int y, int A, int direction) {
   int secondaryX[4] = {-1, 0, 1, 0}; // == targetY[]
   int secondaryY[4] = {0, 1, 0, -1}; // == targetX[]
   int d = direction - 1;
+  //printf("%d\n", buf[2*A*(y+secondaryX[d]) + (x+secondaryY[d])]);
   if (
     (buf[2*A*(y+secondaryX[d]) + (x+secondaryY[d])] == 0) &&
     (buf[2*A*(y+secondaryX[d]+secondaryY[d]) + x+secondaryY[d]+secondaryX[d]] == 0)
   ) {
+    //printf("moving: %d, %d, %d\n", x, y, d);
     buf[2*A*(y+secondaryX[d]) + (x+secondaryY[d])] = d + 5;
     buf[2*A*(y+secondaryX[d]+secondaryY[d]) + x+secondaryY[d]+secondaryX[d]] = 255;
     buf[2*A*y + x] = buf[2*A*(y+secondaryY[d]) + x+secondaryX[d]] = 0;
@@ -86,14 +98,14 @@ int moveTile(uint8_t* buf, int x, int y, int A, int direction) {
 }
 
 int processMovement(uint8_t* buf, int A, int currentA) {
-  for (int y = (A-currentA); y < 2*currentA + 1; y++) {
-    for (int x = (A-currentA); x < 2*currentA + 1; x++) {
-      // int bx = (A-currentA)+x;
-      // int by = (A-currentA)+y;
-      int d = buf[2*A*y + x];
-      printf("%d, %d: %d\n", x, y, d);
+  for (int y = 0; y < 2*currentA; y++) {
+    for (int x = 0; x < 2*currentA; x++) {
+      int bx = (A-currentA)+x;
+      int by = (A-currentA)+y;
+      int d = buf[2*A*by + bx];
+      //printf("bbb %d, %d: %d\n", bx, by, d);
       if (d > 0 && d < 5) {
-        if (currentA < 3)moveTile(buf, x, y, A, d);
+        moveTile(buf, bx, by, A, d);
       }
     }
   }
@@ -111,47 +123,13 @@ int processMovement(uint8_t* buf, int A, int currentA) {
 
 uint8_t generateCircle(uint8_t buf[], uint8_t A, uint8_t seed = 1) {
   srand(seed);
-  uint8_t circleArr[4] = {0};
-  fillSquare(circleArr, 2, 0);
-
-  for (int y = 0; y < 2; y++) {
-    for (int x = 0; x < 2; x++) { 
-      buf[2*A*((A-1)+y) + (A-1)+x] = circleArr[2*y + x];
-      //printf("%d %d %d %d\n", y, x, buf[2*A*y+x], circleArr[2*y + x]);
-    }
-  }
-
+  fillSquare((uint8_t*)(buf + 2*A*(A-1) + (A-1)), 2*A, 0);
   if (A > 1) {
-    for (int currentA = 2; currentA <= A; currentA++) {
-      // int workingArr[currentA*currentA*4] = {0};
-      // for (int y = 0; y < currentA*2; y++) {
-      //   for (int x = 0; x < currentA*2; x++) { 
-      //     workingArr[2*currentA*y + x] = buf[2*A*((A-currentA)+y) + (A-currentA)+x];
-      //     printf(" %d ", workingArr[2*currentA*y + x]);
-      //   }
-      //   printf("\n");
-      // }
-      //printf("-----\n");
-        processMovement(buf, A, currentA);
-      if (currentA < 3) {
-        fillNewSquares(buf, A, currentA);
-      }
+    for (int currentA = 1; currentA < A; currentA++) {
       
-      // for (int y = 0; y < currentA*2; y++) {
-      //   for (int x = 0; x < currentA*2; x++) { 
-      //     buf[2*A*((A-currentA)+y) + (A-currentA)+x] = workingArr[2*currentA*y + x];
-      //     printf(" %d ", workingArr[2*currentA*y + x]);
-      //   }
-      //   printf("\n");
-      // }
+      processMovement(buf, A, currentA);
+      fillNewSquares(buf, A, currentA);
     }
-  }
-  //printf("-----\n");
-  for (int y = 0; y < 2*A; y++) {
-    for (int x = 0; x < 2*A; x++) { 
-      //printf(" %d ", buf[2*A*y+x]);
-    }
-    //printf("\n");
   }
   return 1;
 }
